c299e38e5b7a85555fa7d5787c5b9ebd3359bff5
ANDREAS VLCEK <andreas.vlcek@googlemail.com>
2023-03-23 19:50:27 +0100

    fixed typo

--- src/vec2.h
+++ src/vec2.h
@@ -11,7 +11,7 @@ vec2_t vec2_add(vec2_t* a, vec2_t* b);
 vec2_t vec2_sub(vec2_t* a, vec2_t* b);
 vec2_t vec2_mul(vec2_t* v, float factor);
 vec2_t vec2_div(vec2_t* v, float factor);
-float vec2_length(verc2_t* v);
+float vec2_length(vec2_t* v);
 float vec2_dot(vec2_t* a, vec2_t* b);
 float vec2_cross(vec2_t* a, vec2_t* b);
 void vec2_normalize(vec2_t* v);
