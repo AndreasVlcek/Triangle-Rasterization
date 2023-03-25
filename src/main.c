0e81d5c4b7243069843e7b9b13f97249af7dc141
ANDREAS VLCEK <andreas.vlcek@googlemail.com>
2023-03-25 11:59:42 +0100

    rendering first triangle

--- src/main.c
+++ src/main.c
@@ -31,6 +31,12 @@ void process_input(void) {
 	}
 }
 
+int edge_cross(vec2_t* a, vec2_t* b, vec2_t* p) {
+	vec2_t ab = { b->x - a->x, b->y - a->y };
+	vec2_t ap = { p->x - a->x, p->y - a->y };
+	return ab.x * ap.y - ab.y * ap.x;
+}
+
 void triangle_fill(vec2_t v0, vec2_t v1, vec2_t v2, uint32_t color) {
 	// Find the bounding box with all candidate pixels
 	int x_min = MIN(MIN(v0.x, v1.x), v2.x);
@@ -43,10 +49,15 @@ void triangle_fill(vec2_t v0, vec2_t v1, vec2_t v2, uint32_t color) {
 		for (int x = x_min; x <= x_max; x++) {
 			vec2_t p = {x, y};
 			
-			// TODO:
-			// if (INSIDE_TRIANGLE)	 {
+			int w0 = edge_cross(&v1, &v2, &p);
+			int w1 = edge_cross(&v2, &v0, &p);
+			int w2 = edge_cross(&v0, &v1, &p);
+			
+			bool is_inside = w0 >= 0 && w1 >=0 && w2 >=0;
+			
+			if (is_inside) {
 				draw_pixel(x, y, 0xFF00FF00);
-			//}
+			}
 		}
 	}
 }
